98361fdc8555bb87fc9c39450ab8c8114a9e2946
Kampouse <jpmartel98@gmail.com>
2020-02-08 02:35:37 +0000

    image_location

--- main.lua
+++ main.lua
@@ -4,21 +4,21 @@ vroom  = {}
 simple = {}
 A.john = {}
 same  = {}
-A.john.dog3 = {1,2,3,4,5,5}
-A.john.dog2 = {1,2,3,4,5,5}
-A.john.dog1 = {1,2,3,4,5,5}
-A.john.dog = {1,2,3,4,5,5}
-A.john.wife = {2,1,2,2,3,3}
-A.john.dogY = {2,3,4,5,6,7}
-A.john.wifY = {30,30,300,30,3,4}
+A.image1X = {0}
+A.image2X = {0}
+A.image3X= {0}
+A.image4X = {0}
+A.image5X= {0}
+A.image6X= {0}
+A.image8X = {0}
 
 
-A.john.dogY = {0}
-A.john.dog1Y = {0}
-A.john.dog2Y = {0}
-A.john.dog3Y = {0}
-
-
+A.image1Y = {0}
+A.image2Y = {0}
+A.image3Y = {0}
+A.image4Y = {0}
+A.image5Y = {0}
+local count  = 25
 
 
 
@@ -26,8 +26,8 @@ A.john.dog3Y = {0}
 
 listy = {}
 image = {}
-listA = {A.john.wifY,A.john.dogY,A.john.dog1Y,A.john.dog2Y,A.john.dog3Y}
-list = {A.john.dog,A.john.dog1,A.john.dog2,A.john.dog3,A.john.wife}
+listA = {A.image1Y,A.image2Y,A.image3Y,A.image4Y,A.image5Y}
+list = {A.image1X,A.image2X,A.image3X,A.image4X,A.image5X}
 --convert table to array --
 
 function tabled(tablex,tabley)
@@ -54,20 +54,59 @@ for i = 1,2 do
                                 end
 end
  function love.load()
+    love.physics.setMeter(32)
+    world = love.physics.newWorld(0,12.0*64,true)
+    object = {}
+    object.ground = {}
+    object.ground.body = love.physics.newBody(world,0,420)
+    object.ground.shape = love.physics.newRectangleShape(800,100)
+    object.ground.fixture = love.physics.newFixture(object.ground.body,object.ground.shape)
+
+    object.image1  = {}
+    object.image1.body = love.physics.newBody(world,100,100,"dynamic")
+    object.image1.shape = love.physics.newRectangleShape(100,100)
+    object.image1.fixture = love.physics.newFixture(object.image1.body,object.image1.shape)
+    
+    object.image = {}
+    object.image.body = love.physics.newBody(world,0,0,"kinematic")
+    object.image.shape = love.physics.newRectangleShape(50,50)
+    object.image.fixture = love.physics.newFixture(object.image.body,object.image.shape)
+    object.image["location"] = function(sourisX,SourisY)
+       
+        object.image.body = love.physics.newBody(world,sourisX,SourisY,"kinematic")
+        object.image.shape = love.physics.newRectangleShape(50,50)
+        object.image.fixture = love.physics.newFixture(object.image.body,object.image.shape)
+        end        
+
+
  for i = 1,5 do 
- image[i] = love.graphics.newImage("player"..i..".png")
+ image[i] = love.graphics.newImage("joueur/player"..i..".png")
         end 
 end
 function love.update(dt)
+    world:update(dt)
                 function love.keypressed(key)
+
+                 if key == 'a'  then
+                    object.image1.body:setLinearVelocity(200,100)
+                    object.image1.body:applyForce(100,100)
+
+                 end
+                 if key == 'b' then object.image1.body:setPosition(0,0) end 
                  if key == "1"  then state = "1"
                         print("state enable:", state )
                 end 
                 end
                 function love.mousepressed(x,mouseY, button,istouch)
                 if button == 1 then
-                        if state == "1" then
-               tabled(A.john.dog,A.john.wifY)
+                    
+                        if state == "1" and count > 0  then
+                            local  SourisX,SourisY = love.mouse:getPosition()
+                object.image.location(SourisX,SourisY)
+                print(moseX,mouseY)
+               tabled(A.image2X,A.image2Y)
+                count = count - 1
+                print(count, "left")
               end
                  end
                      end
@@ -78,7 +117,7 @@ function love.draw()
         for i = 1,2 do
                  for j =  1,size[i] do 
                         love.graphics.draw(image[i],list[i][j],listA[i][j])
-                        
+                        love.graphics.draw(image[3],object.image1.body:getPosition())
                         end
                         
                                 end
